6ccf206c92ad1e4d82266b60e3cd7fefc665040f
Jerome Gallego <gallego6@hawaii.edu>
2020-11-16 21:48:41 -1000

    Editing Admin use

--- app/imports/ui/components/VendorItemAdmin.jsx
+++ app/imports/ui/components/VendorItemAdmin.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
-import { Image, Card } from 'semantic-ui-react';
+import { Table, Button, Icon } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff (Admin) table. See pages/ListStuffAdmin.jsx. */
 class VendorItemAdmin extends React.Component {
+
+  removeItem(docID) {
+    // eslint-disable-next-line no-console
+    console.log(`item to delete is: ${docID}`);
+    this.props.Vendors.collection.remove(docID);
+  }
   render() {
     return (
-        <Card centered>
-          <Image src={this.props.vendor.image} wrapped ui={false} />
-          <Card.Content>
-            <Card.Header>{this.props.vendor.name}</Card.Header>
-            <Card.Header>{this.props.vendor.price}</Card.Header>
-            <Card.Meta>
-              <span className='date'>{this.props.vendor.cuisine}</span>
-            </Card.Meta>
-            <Card.Description>
-              Location: {this.props.vendor.location}
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <a>
-              <Link to={`/edit/${this.props.vendor._id}`}>Edit</Link>
-            </a>
-          </Card.Content>
-        </Card>
+        <Table.Row>
+          <Table.Cell>{this.props.vendor.name}</Table.Cell>
+          <Table.Cell>{this.props.vendor.cuisine}</Table.Cell>
+          <Table.Cell>{this.props.vendor.location}</Table.Cell>
+          <Table.Cell>{this.props.vendor.image}</Table.Cell>
+          <Table.Cell>{this.props.vendor.price}</Table.Cell>
+          <Table.Cell>
+            <Link to={`/edit/${this.props.vendor._id}`}>Edit</Link>
+          </Table.Cell>
+          <Table.Cell>
+            <Button icon onClick={() => this.removeItem(this.props.vendor._id)}><Icon name='trash'/>
+            </Button>
+          </Table.Cell>
+        </Table.Row>
     );
   }
 }
@@ -32,6 +34,7 @@ class VendorItemAdmin extends React.Component {
 /** Require a document to be passed to this component. */
 VendorItemAdmin.propTypes = {
   vendor: PropTypes.object.isRequired,
+  Vendors: PropTypes.object.isRequired,
 };
 
 export default withRouter(VendorItemAdmin);
--- app/imports/ui/pages/ListVendorAdmin.jsx
+++ app/imports/ui/pages/ListVendorAdmin.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Header, Loader, Card } from 'semantic-ui-react';
+import { Table, Container, Header, Loader } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Vendors } from '../../api/vendor/Vendor';
@@ -17,28 +17,40 @@ class ListVendorAdmin extends React.Component {
   /** Render the page once subscriptions have been received. */
   renderPage() {
     return (
-        <div className="vendor-list"><Container>
-          <Header as="h2" textAlign="center" inverted>Vendors (Admin)</Header>
-          <Card.Group>
-            {this.props.vendors.map((vendor) => <VendorItemAdmin key={vendor._id} vendor={vendor}/>)}
-          </Card.Group>
-        </Container></div>
-    );
-  }
-}
+        <Container>
+          <Header as="h2" textAlign="center" inverted>Admin</Header>
+          <Table celled>
+            <Table.Row>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Cuisine</Table.HeaderCell>
+              <Table.HeaderCell>Location</Table.HeaderCell>
+              <Table.HeaderCell>Images</Table.HeaderCell>
+              <Table.HeaderCell>Price</Table.HeaderCell>
+              <Table.HeaderCell>Edit</Table.HeaderCell>
+              <Table.HeaderCell>Remove</Table.HeaderCell>
 
-/** Require an array of Stuff documents in the props. */
-ListVendorAdmin.propTypes = {
+            </Table.Row>
+          <Table.Body>
+            {this.props.vendors.map((vendor) => <VendorItemAdmin key={vendor._id} vendor={vendor} Vendors={Vendors}/>)}
+          </Table.Body>
+        </Table>
+        </Container>
+  );
+  }
+  }
+
+  /** Require an array of Stuff documents in the props. */
+  ListVendorAdmin.propTypes = {
   vendors: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
-};
+  };
 
-/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
-export default withTracker(() => {
+  /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+  export default withTracker(() => {
   // Get access to Stuff documents.
   const subscription = Meteor.subscribe(Vendors.adminPublicationName);
   return {
-    vendors: Vendors.collection.find({}).fetch(),
-    ready: subscription.ready(),
+  vendors: Vendors.collection.find({}).fetch(),
+  ready: subscription.ready(),
   };
-})(ListVendorAdmin);
+  })(ListVendorAdmin);

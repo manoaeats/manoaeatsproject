98657314979ec0b6cd649df46ff3ef1ad9a69b69
mkraquiz <mkraquiz@users.noreply.github.com>
2020-11-18 03:43:39 -1000

    updated

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -9,13 +9,13 @@ class Landing extends React.Component {
           <Grid container centered stackable columns={2}>
             <Grid.Column textAlign='center'>
               <Icon size="huge" name="users" inverted/>
-              <Header as='h2' inverted>Users</Header>
+              <Header as='h1' inverted>Users</Header>
               <Header as='h4' inverted>You can search and have a faster way of locating the food that you wanted. You will be able to see the daily top picks and foods available.</Header>
             </Grid.Column>
 
             <Grid.Column textAlign='center'>
               <Icon size="huge" name="users" inverted/>
-              <Header as='h2' inverted>Vendors</Header>
+              <Header as='h1' inverted>Vendors</Header>
               <Header as='h4' inverted>Add your business for the users to see your location, hours of operation, and menu.</Header>
             </Grid.Column>
           </Grid>

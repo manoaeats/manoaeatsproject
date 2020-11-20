dd4604eec0438f5ed5556911520bd1b8206d1010
Christine Kaye Guira <ceekaye12@users.noreply.github.com>
2020-11-20 03:54:38 -1000

    Updated User Homepage

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import { Container, Menu, Icon } from 'semantic-ui-react';
+import { Icon } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px', color: 'white' };
+    const divStyle = { paddingTop: '25px', color: 'white' };
     return (
           <footer>
-            <Menu borderless className="ui center aligned footerMenu">
-              <Container>
-                <Menu borderless className="ui center aligned footerMenu">
-                  <Container>
-                    <Menu.Item fitted position="centered"><Icon name="facebook f"/></Menu.Item>
-                    <Menu.Item fitted><Icon name="twitter"/></Menu.Item>
-                    <Menu.Item fitted><Icon name="instagram"/></Menu.Item>
-                  </Container>
-                </Menu>
-              </Container>
-            </Menu>
             <div style={divStyle} className="ui center aligned container">
+              <hr />
+              <Icon name="facebook f"/>
+              <Icon name="twitter"/>
+              <Icon name="instagram"/><br/>
               Email: manoaeats@hawaii.edu <br/>
               Phone: (808) 123-4567<br/>
             </div>
--- app/imports/ui/pages/UserHomePage.jsx
+++ app/imports/ui/pages/UserHomePage.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Grid, Header } from 'semantic-ui-react';
+import { Grid, Header, Search } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class UserHomePage extends React.Component {
   render() {
     return (
         <div className="manoaeats-UserHomePage-background">
-          <Grid container centered >
-            <Header as='h2'>Aloha!</Header>
+          <Grid container centered stackable columns={1}>
+            <Grid.Column textAlign='center'>
+              <div className={'user-home'}>Aloha!</div>
+              <Search className={'search-bar'}></Search>
+            </Grid.Column>
           </Grid>
         </div>
     );
